667e3244407b83a7446d6f7ddca905cba87f7511
maugares <mauro.garciaesteban@gmail.com>
2019-03-29 14:12:17 +0100

    HTTP Clients section finished

--- HTTP_Clients.js
+++ HTTP_Clients.js
@@ -17,8 +17,21 @@ console.log the name of the planet. Do all of this in a single chain of promises
 
 const fetch = require("node-fetch")
 
-const fetchObject = fetch("https://swapi.co/api/people/3/")
+fetch("https://swapi.co/api/people/3/")
   .then(response => response.json())
-  .then(planet => console.log(`URL: ${planet.url} \nPlanet name: ${planet.name}`))
+  .then(obj => {
+    fetch(obj.homeworld)
+      .then(response2 => response2.json())
+      .then(obj2 => console.log(`The name of the planet is ${obj2.name}`))
+      .catch(error => console.log(`There was an error`))
+  }
+  )
 
 
+// Using SuperAgent for fetching information
+
+const superagent = require("superagent")
+
+superagent.get('https://swapi.co/api/films/1/')
+  .then(response => console.log(response.body))
+  .catch(error => console.log(`error`))
